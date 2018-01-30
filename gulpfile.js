74e9d85d55eb52fd3f3badfcbdbae5e821f2ee38
shihangbo <shihangbo@126.com>
2018-01-30 11:46:31 +0800

    update_add: 完成使用gulp构建出dist文件的开发流；

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+
+// 引入依赖
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    rename = require('gulp-rename')
+    minifycss = require('gulp-minify-css')
+
+// 定义变量
+var DEST = 'dist/'
+
+// 创建task
+gulp.task('testLess', function() {
+  gulp.src('src/csskit.less')
+      .pipe(less())
+      .pipe(gulp.dest(DEST))
+      .pipe(minifycss())
+      .pipe(rename({ extname: '.min.js' }))
+      .pipe(gulp.dest(DEST));
+})
+
+gulp.task('default', ['testLess'])
